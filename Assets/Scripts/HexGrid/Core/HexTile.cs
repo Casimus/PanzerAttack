1a55ef01092cfb859f6b030c5d3bff229ec4245a
Casimus <grzegorz.kazimierczak@wp.pl>
2026-04-25 11:13:08 +0200

    Add initial Unity project configuration files.

---
+++ Assets/Scripts/HexGrid/Core/HexTile.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace PanzerAttack.HexGrid.Core
+{
+    public class HexTile : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
